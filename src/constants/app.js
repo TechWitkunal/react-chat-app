697760c5ac7be4d42c40e663623f81f75e71d86c
Kunal <techlearning1818@gmail.com>
2024-04-28 12:11:37 +0530

    Updateing app url

--- src/constants/app.js
+++ src/constants/app.js
@@ -1,4 +1,4 @@
-export const clientPath = "http://localhost:3000" 
-// export const clientPath = "https://main--online-chat-app-0011.netlify.app" 
-export const serverPath = "http://localhost:8000" 
-// export const serverPath = "https://chat-app-server-ojsr.onrender.com" 
+// export const clientPath = "http://localhost:3000" 
+export const clientPath = "https://main--online-chat-app-0011.netlify.app" 
+// export const serverPath = "http://localhost:8000" 
+export const serverPath = "https://chat-app-server-ojsr.onrender.com" 
